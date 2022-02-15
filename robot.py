e67694609044b9e7d8086a5586171846741710bf
sean mabli <70709738+seanmabli@users.noreply.github.com>
2022-02-15 22:54:28 +0000

    teleop functions added almost done

--- robot.py
+++ robot.py
@@ -1,66 +1,77 @@
-import wpilib
 import wpilib.drive
 import ctre
 from constants import constants
 from networktables import NetworkTables
 import navx
 
+'''
+Left bumper: Lock straight
+Right bumper: Lock reverse
+Left and right bumpers: Lock straight backwards
+D-pad left/right: Turn 90 deg left or right
+X: Taunt
+'''
+
 class MyRobot(wpilib.TimedRobot):
+    def output(text, value):
+      print(text + ': ' + value)      
+      self.sd.putValue(text, value)
+      
     def robotInit(self):
         self.front_left_motor = ctre.WPI_TalonSRX(constants["frontLeftPort"])
         self.rear_left_motor = ctre.WPI_VictorSPX(constants["rearLeftPort"])
         self.rear_left_motor.setInverted(True)
-        self.left = wpilib.SpeedControllerGroup(
-            self.front_left_motor, self.rear_left_motor)
+        self.left = wpilib.SpeedControllerGroup(self.front_left_motor, self.rear_left_motor)
 
         self.front_right_motor = ctre.WPI_TalonSRX(constants["frontRightPort"])
         self.rear_right_motor = ctre.WPI_VictorSPX(constants["rearRightPort"])
         self.rear_right_motor.setInverted(True)
-        self.right = wpilib.SpeedControllerGroup(
-            self.front_right_motor, self.rear_right_motor)
+        self.right = wpilib.SpeedControllerGroup(self.front_right_motor, self.rear_right_motor)
 
-        self.drive = wpilib.drive.DifferentialDrive(
-            self.right,
-            self.left
-        )
+        self.drive = wpilib.drive.DifferentialDrive(self.right, self.left)
 
         self.controller = wpilib.XboxController(0)
         self.timer = wpilib.Timer()
         self.sd = NetworkTables.getTable("SmartDashboard")
         self.gyro = navx.AHRS.create_i2c()
-        self.front_left_motor.configSelectedFeedbackSensor(
-            ctre.FeedbackDevice.QuadEncoder, 0, 0)
-        self.front_right_motor.configSelectedFeedbackSensor(
-            ctre.FeedbackDevice.QuadEncoder, 0, 0)
+        self.front_left_motor.configSelectedFeedbackSensor(ctre.FeedbackDevice.QuadEncoder, 0, 0)
+        self.front_right_motor.configSelectedFeedbackSensor(ctre.FeedbackDevice.QuadEncoder, 0, 0)
 
     def autnomousInit(self):
         self.timer.reset()
         self.timer.start()
-
+  
     def autonomousPeriodic(self):
-        print("Time: ", self.timer.get())
+        output('Time', self.timer.get())
 
     def teleopInit(self):
         print("Starting teleop...")
-
+        self.mode = 'individual'
+        self.speed =  [1, 1]
+  
     def teleopPeriodic(self):
-        print("The drive X value is: ", self.controller.getX(
-            self.controller.Hand.kLeftHand))
-        print("The drive Y value is: ", self.controller.getY(
-            self.controller.Hand.kLeftHand))
-        print("The gyro Yaw value is: ", self.gyro.getYaw())
-        self.sd.putValue("Gyro Yaw", self.gyro.getYaw())
-        self.sd.putValue("Left Encoder Value",
-                         self.front_left_motor.getSelectedSonsorPosition())
-        self.sd.putValue("Right Encoder Value",
-                         self.front_right_motor.getSelectedSensorPosition())
-        self.drive.arcadeDrive(
-        	self.controller.getX(self.controller.Hand.kLeftHand),
-        	self.controller.getY(self.controller.Hand.kLeftHand),
-        	True
-        )
+        output('Drive X', self.controller.getX(self.controller.Hand.kLeftHand))
+        output('Drive Y', self.controller.getY(self.controller.Hand.kLeftHand))
+        output('Gyro Yaw', self.gyro.getYaw())
+        output('Left Encoder', self.front_left_motor.getSelectedSonsorPosition())
+        output('Right Encoder', self.front_right_motor.getSelectedSensorPosition())
+        
+        self.speed = [1, 1] if self.controller.getAButton() else self.speed # half speed 
+        self.speed = [1, 1] if self.controller.getBButton() else self.speed # quarter speed
+        self.speed = [1, 1] if self.controller.getYButton() else self.speed # inch speed
+        self.mode = 'straight' if self.controller.getLeftBumperPressed() else self.mode # 
+        self.mode = 'backward' if self.controller.getRightBumperPressed() else self.mode # 
+        
+        self.drive.arcadeDrive(self.controller.getX(self.controller.Hand.kLeftHand) * self.speed[0], self.controller.getY(self.controller.Hand.kLeftHand) * self.speed[1])
+        
+      def turnleft90():
+        yaw = self.gyro.getYaw()
+        self.speed = [(1 - ((self.gyro.getYaw() - yaw)/90) ** 4, 1 + ((self.gyro.getYaw() - yaw)/90) ** 4]
 
+      def turnleft90():
+        yaw = self.gyro.getYaw()
+        self.speed = [(1 + ((self.gyro.getYaw() - yaw)/90) ** 4, 1 - ((self.gyro.getYaw() - yaw)/90) ** 4]
 
 if __name__ == "__main__":
-    wpilib.run(MyRobot)
-
+  wpilib.run(MyRobot)
+  
