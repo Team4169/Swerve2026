2ba7757900663c142b986d4c861d5be904324d9f
AsherL <asher.leavitt@gmail.com>
2022-03-03 17:06:35 -0500

    changed name to work with robot

--- commands/newPath.py
+++ commands/newPath.py
@@ -16,5 +16,14 @@ class newPath(commands2.SequentialCommandGroup):
     def __init__(self, drive: DriveSubsystem):
         super().__init__(
             # Drive forward the specified distance
-            ResetGyro(drive)
+            ResetGyro(drive),
+            MoveCommand(3.75, 0, drive),
+            #pick up balls
+            MoveCommand(0, 180, drive),
+            MoveCommand(9, 180, drive),
+            MoveCommand(0, 195, drive),
+            MoveCommand(1.5, 195, drive),
+            #drop balls
+            MoveCommand(-1.5, 195, drive),
+            
             )
