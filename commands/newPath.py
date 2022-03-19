7a9d7c818b30ebc48e2d845f68456c184009eaf5
AsherL <asher.leavitt@gmail.com>
2022-03-19 17:17:50 -0400

    changed the command before dropoff on both newpaths to be less in case of bug where movecommand was not completing

--- commands/newPath.py
+++ commands/newPath.py
@@ -25,7 +25,7 @@ class newPath(commands2.SequentialCommandGroup):
             MoveCommand(0, 180, drive),
             MoveCommand(7, 180, drive),
             MoveCommand(0, 220, drive),
-            MoveCommand(1, 220, drive),
+            MoveCommand(.75, 220, drive),
             dropOff(1, 1, snowveyor),
             MoveCommand(-1, 220, drive),
 
