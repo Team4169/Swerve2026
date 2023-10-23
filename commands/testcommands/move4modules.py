231b67deb40501751741b33f2a247ba074f30d65
AsherL <asher.leavitt@gmail.com>
2023-10-23 12:56:38 -0400

    slight change to move4modules

--- commands/testcommands/move4modules.py
+++ commands/testcommands/move4modules.py
@@ -10,48 +10,16 @@ class move4modules(commands2.CommandBase):
     def __init__(self, swerve: SwerveSubsystem) -> None:
         super().__init__()
         self.swerve = swerve
-        # self.frontLeft = swervemodule(RobotConstants.frontLeftDrivingMotorID, 
-        #                         RobotConstants.frontLeftTurningMotorID, 
-        #                         RobotConstants.frontLeftDrivingMotorReversed, 
-        #                         RobotConstants.frontLeftTurningMotorReversed, 
-        #                         RobotConstants.frontLeftAbsoluteEncoderId, 
-        #                         RobotConstants.frontLeftAbsoluteEncoderOffset, 
-        #                         RobotConstants.frontLeftAbsoluteEncoderReversed)
-        
-        self.frontRight = swervemodule(RobotConstants.frontRightDrivingMotorID, 
-                                RobotConstants.frontRightTurningMotorID, 
-                                RobotConstants.frontRightDrivingMotorReversed, 
-                                RobotConstants.frontRightTurningMotorReversed, 
-                                RobotConstants.frontRightAbsoluteEncoderId, 
-                                RobotConstants.frontRightAbsoluteEncoderOffset, 
-                                RobotConstants.frontRightAbsoluteEncoderReversed)
-        
-        self.backLeft = swervemodule(RobotConstants.backLeftDrivingMotorID, 
-                                RobotConstants.backLeftTurningMotorID, 
-                                RobotConstants.backLeftDrivingMotorReversed, 
-                                RobotConstants.backLeftTurningMotorReversed, 
-                                RobotConstants.backLeftAbsoluteEncoderId, 
-                                RobotConstants.backLeftAbsoluteEncoderOffset, 
-                                RobotConstants.backLeftAbsoluteEncoderReversed)
-        
-        self.backRight = swervemodule(RobotConstants.backRightDrivingMotorID, 
-                                RobotConstants.backRightTurningMotorID, 
-                                RobotConstants.backRightDrivingMotorReversed, 
-                                RobotConstants.backRightTurningMotorReversed, 
-                                RobotConstants.backRightAbsoluteEncoderId, 
-                                RobotConstants.backRightAbsoluteEncoderOffset, 
-                                RobotConstants.backRightAbsoluteEncoderReversed)
-        
         self.startTime = time.time()
         self.runTime = 5
 
 
     def initialize(self):
         self.rotation = Rotation2d(1.0, 1.0)
-        self.frontLeft.setDesiredState(SwerveModuleState(0.25, self.rotation))
-        self.frontRight.setDesiredState(SwerveModuleState(0.25, self.rotation))
-        self.backLeft.setDesiredState(SwerveModuleState(0.25, self.rotation))
-        self.backRight.setDesiredState(SwerveModuleState(0.25, self.rotation))
+        self.swerve.frontLeft.setDesiredState(SwerveModuleState(0.25, self.rotation))
+        self.swerve.frontRight.setDesiredState(SwerveModuleState(0.25, self.rotation))
+        self.swerve.backLeft.setDesiredState(SwerveModuleState(0.25, self.rotation))
+        self.swerve.backRight.setDesiredState(SwerveModuleState(0.25, self.rotation))
 
     def execute(self) -> None:
         self.sd.putNumber("Module State", self.frontLeft.getState())
